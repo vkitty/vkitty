a55dae0f08a9f7e51af8462e519cafdbfab45f98
付东光 <fudongguang@weidian.com>
2017-05-19 00:57:57 +0800

    0.1.7 ok

--- test/test.js
+++ test/test.js
@@ -46,15 +46,17 @@ function compare(){
 
     setTimeout(function(){
         equalFile('./build/pages/mini/index.html','./demo/compare/mini/compare.html');
-        //equalFile('./build/pages/require/index.html','./demo/compare/require/compare.html');
-        //equalFile('./build/pages/options/index.html','./demo/compare/options/compare.html');
-        //equalFile('./build/pages/cdn/index.html','./demo/compare/cdn/compare.html');
-        //equalFile('./build/static/test.ogg','./demo/compare/cdn/media.less');
-        //equalFile('./build/pages/text/index.html','./demo/compare/text/compare.html');
-        //equalFile('./build/static/aaaaa.css','./demo/compare/cdn/compare.less');
-        //equalFile('./build/pages/common/index.html','./demo/compare/common/compare.html');
-        //equalFile('./build/pages/comments/index.html','./demo/compare/comments/compare.html');
-        //equalFile('./build/pages/srcs/src1.html','./demo/compare/srcs/compare/src1.html');
+        equalFile('./build/pages/require/index.html','./demo/compare/require/compare.html');
+        equalFile('./build/pages/options/index.html','./demo/compare/options/compare.html');
+        equalFile('./build/pages/cdn/index.html','./demo/compare/cdn/compare.html');
+        equalFile('./build/static/test.ogg','./demo/compare/cdn/media.less');
+        equalFile('./build/pages/text/index.html','./demo/compare/text/compare.html');
+        equalFile('./build/static/aaaaa.css','./demo/compare/cdn/compare.less');
+        equalFile('./build/pages/common/index.html','./demo/compare/common/compare.html');
+        equalFile('./build/pages/comments/index.html','./demo/compare/comments/compare.html');
+        equalFile('./build/pages/srcs/src1.html','./demo/compare/srcs/compare/src1.html');
+        equalFile('./build/pages/srcs/src2.html','./demo/compare/srcs/compare/src2.html');
+        equalFile('./build/pages/srcs/src3.html','./demo/compare/srcs/compare/src3.html');
     },1000);
 }
 
